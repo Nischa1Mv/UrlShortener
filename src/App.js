b9c37ae2270328393500a642fdb7b390f9cc344b
Nischal <sainischalmv@gmail.com>
2024-08-03 22:07:26 +0530

    front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="w-screen h-screen flex flex-col ">
+        <div className="text-6xl font-bold text-center py-6 ">
+          Url Shortener
+        </div>
+
+        <form
+          type="submit"
+          className="flex flex-col w-full  justify-center items-center gap-5  flex-1"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="w-[85%] ">
+            <input
+              className="w-full border-black border rounded-lg px-2 py-4 focus:outline-none"
+              placeholder="Paste Your Url Here"
+              type="text"
+            />
+          </div>
+          <div>
+            <input
+              className="border bg-red-300 px-4 py-2 rounded-full hover:bg-red-400 hover:text-white text-xl font-semibold"
+              type="submit"
+            />
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
---
+++ src/firebasae.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDprA_r0em8aEU9E7xSyuRAi-ZwZezxYVU",
+  authDomain: "urlshortener-9194a.firebaseapp.com",
+  projectId: "urlshortener-9194a",
+  storageBucket: "urlshortener-9194a.appspot.com",
+  messagingSenderId: "887232768236",
+  appId: "1:887232768236:web:3ef236f995ed8b2be21dc9",
+};
+
+const app = initializeApp(firebaseConfig);
